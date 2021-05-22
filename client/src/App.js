2df0600c488a59ecf4a98604b2d47723fb3194bf
Ankush Dogra <ankushdogradev@gmail.com>
2021-05-22 19:54:19 +0530

    Added Footer

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,14 @@
 import React from "react";
 import Navbar from "./components/Header/Navbar/Navbar";
 import Footer from "./components/Footer/Footer";
+import Body from "./components/Body/Body";
 import "./App.css";
 
 const App = () => {
   return (
     <div>
       <Navbar />
-
+      <Body />
       <Footer />
     </div>
   );
--- client/src/components/Footer/Footer.js
+++ client/src/components/Footer/Footer.js
@@ -2,7 +2,56 @@ import React from "react";
 import "./Footer.css";
 
 const Footer = () => {
-  return <></>;
+  return (
+    <>
+      <footer>
+        {/* Main Footer */}
+        <div className="foo-main">
+          <div className="foo-main-col">
+            <img className="foo-main-logo" src={"/logo/logo.svg"} alt="Logo" />
+          </div>
+          <div className="foo-main-col">
+            <h2>Get to know us</h2>
+            <ul>
+              <li>
+                <a href="#">About us</a>
+              </li>
+              <li>
+                <a href="#">Shipping</a>
+              </li>
+              <li>
+                <a href="#">Contact Us</a>
+              </li>
+            </ul>
+          </div>
+          <div className="foo-main-col">
+            <h2>Contact Us</h2>
+            <ul>
+              <li>
+                <a href="#">Github</a>
+              </li>
+              <li>
+                <a href="#">linkedin</a>
+              </li>
+              <li>
+                <a href="#">Email</a>
+              </li>
+            </ul>
+          </div>
+        </div>
+
+        {/* Second Footer */}
+        <div className="fo-sec">
+          <div className="foo-sec-row">
+            <p className="foo-sec-legal">
+              &copy;{new Date().getFullYear()} Designed & programmed by Ankush
+              Dogra | CC: Creative Commons
+            </p>
+          </div>
+        </div>
+      </footer>
+    </>
+  );
 };
 
 export default Footer;
