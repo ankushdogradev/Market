8e4bfe20c2e4788c656869f94ba9efdeae2df167
Ankush Dogra <ankushdogradev@gmail.com>
2021-06-01 20:20:08 +0530

    Added redux to HomeScreen.js

--- client/src/screens/HomeScreen/HomeScreen.js
+++ client/src/screens/HomeScreen/HomeScreen.js
@@ -1,31 +1,43 @@
-import React, { useState, useEffect } from "react";
-import axios from "axios";
+import React, { useEffect } from "react";
 import ProductCard from "../../components/ProductCard/ProductCard";
+import { useDispatch, useSelector } from "react-redux";
+import { listProducts } from "../../redux/actions/productActions";
 import "./HomeScreen.scss";
 
 const HomeScreen = () => {
-  const [products, setProducts] = useState([]);
+  const dispatch = useDispatch();
 
-  //   As soon as component runs this will run
+  //  here we are taking data from productList reducer stored in store.js
+  const productList = useSelector((state) => state.productList);
+  const { loading, error, products } = productList;
+
+  //   As soon as component runs, useEffect() run's
   useEffect(() => {
-    const fetchProducts = async () => {
-      const { data } = await axios.get("/api/products");
-      setProducts(data.products);
-    };
-
-    fetchProducts();
-  }, []);
+    // Fire/calls listProducts:Action/funtion
+    dispatch(listProducts());
+  }, [dispatch]);
 
   return (
     <>
       <h1 className="heading">Best Selling Products</h1>
-      <div className="prod-row">
-        {products.map((product) => (
-          <div className="prod-col" key={product._id}>
-            <ProductCard product={product} />
-          </div>
-        ))}
-      </div>
+
+      {/* If loading true -> loading icon, 
+      else if error -> show error, 
+      else show & pass data in product card */}
+
+      {loading ? (
+        <h2>Loading...</h2>
+      ) : error ? (
+        <h3>{error}</h3>
+      ) : (
+        <div className="prod-row">
+          {products.map((product) => (
+            <div className="prod-col" key={product._id}>
+              <ProductCard product={product} />
+            </div>
+          ))}
+        </div>
+      )}
     </>
   );
 };
