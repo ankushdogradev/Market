2a160280bbf5836293dc1a00129eb3d6b5e0fb6b
Ankush Dogra <ankushdogradev@gmail.com>
2021-06-02 12:57:07 +0530

    Added redux to product screen

--- client/src/redux/actions/productActions.js
+++ client/src/redux/actions/productActions.js
@@ -4,9 +4,12 @@ import {
   PRODUCT_LIST_REQUEST,
   PRODUCT_LIST_SUCCESS,
   PRODUCT_LIST_FAIL,
+  PRODUCT_DETAILS_REQUEST,
+  PRODUCT_DETAILS_SUCCESS,
+  PRODUCT_DETAILS_FAIL,
 } from "./../constants/productConstants";
 
-// action reducer
+// Product List: action creator
 export const listProducts = () => async (dispatch) => {
   try {
     dispatch({ type: PRODUCT_LIST_REQUEST });
@@ -17,6 +20,24 @@ export const listProducts = () => async (dispatch) => {
   } catch (error) {
     dispatch({
       type: PRODUCT_LIST_FAIL,
+      payload:
+        error.response && error.response.data.message
+          ? error.response.data.message
+          : error.message,
+    });
+  }
+};
+
+// Product: action creator
+export const detailProduct = (props) => async (dispatch) => {
+  try {
+    dispatch({ type: PRODUCT_DETAILS_REQUEST });
+
+    const { data } = await axios.get(`/api/product/${props}`);
+    dispatch({ type: PRODUCT_DETAILS_SUCCESS, payload: data.product });
+  } catch (error) {
+    dispatch({
+      type: PRODUCT_DETAILS_FAIL,
       payload:
         error.response && error.response.data.message
           ? error.response.data.message
--- client/src/screens/ProductScreen/ProductScreen.js
+++ client/src/screens/ProductScreen/ProductScreen.js
@@ -1,44 +1,53 @@
-import React, { useState, useEffect } from "react";
-import axios from "axios";
+import React, { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { detailProduct } from "./../../redux/actions/productActions";
+import Loader from "../../components/Loader/Loader";
+import ErrorMessage from "../../components/ErrorMessage/ErrorMessage";
 import "./ProductScreen.scss";
 
 const ProductScreen = ({ match }) => {
-  const [product, setProduct] = useState({});
+  const dispatch = useDispatch();
+
+  const productDetails = useSelector((state) => state.productDetails);
+
+  const { loading, error, product } = productDetails;
 
   useEffect(() => {
-    const fetchProduct = async () => {
-      const { data } = await axios.get(`/api/product/${match.params.id}`);
-      setProduct(data.product);
-    };
-    fetchProduct();
-  }, [match.params.id]);
+    dispatch(detailProduct(match.params.id));
+  }, [dispatch, match]);
 
   return (
     <>
-      <div className="product-container">
-        <div className="product-image">
-          <img src={product.image} alt="Product" />
+      {loading ? (
+        <Loader />
+      ) : error ? (
+        <ErrorMessage error={error} />
+      ) : (
+        <div className="product-container">
+          <div className="product-image">
+            <img src={product.image} alt="Product" />
+          </div>
+          <div className="product-content">
+            <h1>{product.name}</h1>
+            <h3>{`Price: ₹${product.price}`}</h3>
+            <h3>{`Rating: ${product.rating}`}</h3>
+            {product.countInStock > 0 ? (
+              <h4 className="product-instock">In Stock</h4>
+            ) : (
+              <h4 className="product-nostock">Out of Stock</h4>
+            )}
+            {product.countInStock > 0 ? (
+              <div className="product-inline">
+                <p>Qty Select</p>
+                <button>Add to Cart</button>{" "}
+              </div>
+            ) : (
+              <div className="product-inline">"</div>
+            )}
+            <p>{product.description}</p>
+          </div>
         </div>
-        <div className="product-content">
-          <h1>{product.name}</h1>
-          <h3>{`Price: ₹${product.price}`}</h3>
-          <h3>{`Rating: ${product.rating}`}</h3>
-          {product.countInStock > 0 ? (
-            <h4 className="product-instock">In Stock</h4>
-          ) : (
-            <h4 className="product-nostock">Out of Stock</h4>
-          )}
-          {product.countInStock > 0 ? (
-            <div className="product-inline">
-              <p>Qty Select</p>
-              <button>Add to Cart</button>{" "}
-            </div>
-          ) : (
-            <div className="product-inline">"</div>
-          )}
-          <p>{product.description}</p>
-        </div>
-      </div>
+      )}
     </>
   );
 };
