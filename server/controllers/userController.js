e63a604664ad024b4705ef703d7cb19335ce0aa9
Ankush Dogra <ankushdogradev@gmail.com>
2021-06-07 18:20:25 +0530

    Added User Login and User Profile request

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -6,10 +6,11 @@
 */
 
 const User = require("../models/userModel");
+const generateToken = require("../utils/generateToken");
 
 //  @description: Auth user & Get token
-//  @route: GET /api/users/login
-//  @access: Public
+//  @route:       POST /api/users/login
+//  @access:      Public
 exports.authUser = async (req, res, next) => {
   try {
     const { email, password } = req.body;
@@ -21,10 +22,29 @@ exports.authUser = async (req, res, next) => {
         name: user.name,
         email: user.email,
         isAdmin: user.isAdmin,
-        token: null,
+        token: generateToken(user._id),
       });
     }
   } catch (error) {
     next(new Error("Invalid email or password"));
+  }
+};
+
+//  @description: Get user profile
+//  @route:       GET /api/users/profile
+//  @access:      Private
+exports.getUserProfile = async (req, res, next) => {
+  const user = await User.findById(req.user._id);
+
+  if (user) {
+    res.json({
+      _id: user._id,
+      name: user.name,
+      email: user.email,
+      isAdmin: user.isAdmin,
+    });
+  } else {
+    res.status(404);
+    next(new Error("User not found"));
   }
 };
