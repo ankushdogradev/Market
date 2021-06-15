9ebd325121701279db8e8d92f5e56b9eeff1119b
Ankush Dogra <ankushdogradev@gmail.com>
2021-06-15 12:44:38 +0530

    Added user name: Drop down to navbar when logged in

--- client/src/components/Header/Navbar/Navbar.js
+++ client/src/components/Header/Navbar/Navbar.js
@@ -1,18 +1,43 @@
-// TODO:
-// Delete MenuItems.JS
-
-import React, { useState } from "react";
+import React, { useState, useRef, useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
 import { Link } from "react-router-dom";
-// import { MenuItems } from "../MenuItems/MenuItems";
 import SearchBar from "../SearchBar/SearchBar";
 import "./Navbar.scss";
 
 const Navbar = (props) => {
   const [clicked, setClicked] = useState(false);
+  const [drop, setDrop] = useState(false);
+  const dropdownRef = useRef(null);
 
-  const handleClick = () => {
+  const menuClick = () => {
     setClicked(!clicked);
   };
+
+  const dropClick = () => {
+    setDrop(!drop);
+  };
+
+  const userLogin = useSelector((state) => state.userLogin);
+  const { userInfo } = userLogin;
+
+  useEffect(() => {
+    const pageClickEvent = (e) => {
+      if (
+        dropdownRef.current !== null &&
+        !dropdownRef.current.contains(e.target)
+      ) {
+        setDrop(!drop);
+      }
+    };
+
+    if (drop) {
+      window.addEventListener("click", pageClickEvent);
+    }
+
+    return () => {
+      window.removeEventListener("click", pageClickEvent);
+    };
+  }, [drop]);
 
   return (
     <>
@@ -21,7 +46,7 @@ const Navbar = (props) => {
           <img className="nav-logo" src={"/logo/logo.svg"} alt="Logo" />
         </Link>
 
-        <div className="nav-menu-icon" onClick={handleClick}>
+        <div className="nav-menu-icon" onClick={menuClick}>
           <i
             className={`${
               clicked ? "fas fa-2x fa-times" : "fas fa-2x fa-bars"
@@ -37,23 +62,36 @@ const Navbar = (props) => {
               <i className="fas fa-shopping-cart"></i> Cart
             </button>
           </Link>
-          <Link to={`/login`} className="nav-Link">
-            <button className="nav-links">Login</button>
-          </Link>
+          {userInfo ? (
+            <div className="nav-drop-container">
+              <button className="nav-drop" onClick={dropClick}>
+                {userInfo.name}
+              </button>
+              <div
+                ref={dropdownRef}
+                className={`nav-drop-content ${drop ? "active" : "inactive"}`}
+              >
+                <ul>
+                  <li id="item1">
+                    <h4>PROFILE</h4>
+                  </li>
+                  <li id="item2">
+                    <h4>LOGOUT</h4>
+                  </li>
+                </ul>
+              </div>
+            </div>
+          ) : (
+            <div>
+              <Link to={`/login`} className="nav-Link">
+                <button className="nav-links">Login</button>
+              </Link>
 
-          <Link to={`/signup`} className="nav-Link">
-            <button className="nav-links">Sign Up</button>
-          </Link>
-
-          {/* {MenuItems.map((item, index) => {
-            return (
-              <li key={index}>
-                <button className={item.cName} href={item.url}>
-                  {item.title}
-                </button>
-              </li>
-            );
-          })} */}
+              <Link to={`/signup`} className="nav-Link">
+                <button className="nav-links">Sign Up</button>
+              </Link>
+            </div>
+          )}
         </ul>
       </nav>
     </>
