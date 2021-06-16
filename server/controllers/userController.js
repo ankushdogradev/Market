c14d1557c9c3af8a72d3d6ba4ef94038b7cd1967
Ankush Dogra <ankushdogradev@gmail.com>
2021-06-16 13:16:34 +0530

    Added Update user profile controller

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -86,3 +86,35 @@ exports.getUserProfile = async (req, res, next) => {
     next(error);
   }
 };
+
+//  @description: Update user profile
+//  @route:       PUT /api/users/profile
+//  @access:      Private
+exports.updateUserProfile = async (req, res, next) => {
+  try {
+    const user = await User.findById(req.user._id);
+
+    if (user) {
+      user.name = req.body.name || user.name;
+      user.email = req.body.email || user.email;
+
+      if (req.body.password) {
+        user.password = req.body.password;
+      }
+
+      const updatedUser = await user.save();
+
+      res.json({
+        _id: updatedUser._id,
+        name: updatedUser.name,
+        email: updatedUser.email,
+        isAdmin: updatedUser.isAdmin,
+        token: generateToken(updatedUser._id),
+      });
+    }
+  } catch (error) {
+    error = new Error("User not found");
+    error.status = 404;
+    next(error);
+  }
+};
